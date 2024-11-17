db12887fdc34fe7dadc0335f9e0999eafad78880
Loïc Delon <l.delon@proton.me>
2024-11-17 20:30:56 +0100

    Code refactor

--- app/job-manager/page.tsx
+++ app/job-manager/page.tsx
@@ -5,8 +5,6 @@ export default async function Page() {
     const data = await fetch('http://backend:8080/jobs/')
     const jobs = await data.json()
 
-
-
     return (
         <ul>
             {jobs.map((job: { id: Key | null | undefined; }) => (
