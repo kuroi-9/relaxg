09627d7027a8c4be64cfd8ff01cdc33a10ae9900
Loïc Delon <l.delon@proton.me>
2024-12-15 18:16:51 +0100

    - Replaced local API by an old version of it from GCloud - Locked versions of dependencies - Filled README.md

--- app/job-manager/page.tsx
+++ app/job-manager/page.tsx
@@ -2,7 +2,9 @@ import JobCard from "@/components/jobCard";
 import {Key} from "react";
 
 export default async function Page() {
-    const data = await fetch('http://backend:8080/jobs/')
+    //API is being rewriten, fetching its old state from gcloud run
+    //const data = await fetch('http://backend:8080/jobs/')
+    const data = await fetch('https://relax-api-190456347501.us-central1.run.app/jobs/')
     const jobs = await data.json()
 
     return (
