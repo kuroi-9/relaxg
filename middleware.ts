26767c9db896f07d515bc5fc020482bb99085133
Loïc Delon <l.delon@proton.me>
2025-03-31 15:01:57 +0200

    Fixed: Blocked account-settings route in middleware

--- middleware.ts
+++ middleware.ts
@@ -8,11 +8,11 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/handler/sign-in', request.url));
     }
 
-    if (request.nextUrl.pathname.startsWith('/handler')) {
-      return NextResponse.redirect(new URL('/app/profile', request.url))
+    if (request.nextUrl.pathname.startsWith('/handler/account-settings')) {
+      return NextResponse.redirect(new URL('/app/jobs-manager', request.url))
     }
 
     return NextResponse.next();
 }
 
-export const config = { matcher: ['/app/:path*'] }
+export const config = { matcher: ['/app/:path*', '/handler/account-settings'] }
