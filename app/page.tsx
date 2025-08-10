a27c15ceb9c49b5eb6c1e4ec6dbb60cbb9afba6c
Loïc Delon <l.delon@proton.me>
2025-08-10 23:35:04 +0200

    Removed dead code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 export const dynamic = "force-static";
 
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default function Home() {
     console.log("Redirecting...");
     redirect("https://www.loicdelon.fr");
-
-    return <></>;
 }
