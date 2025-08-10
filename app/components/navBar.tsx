46f1cdc08e5d6fa1a7f06cb529641bdeeb9719de
Loïc Delon <l.delon@proton.me>
2025-08-10 18:28:19 +0200

    Added loading bar on nav items

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import React from "react";
 import { usePathname } from "next/navigation";
-import '@/app/globals.css'
+import "@/app/globals.css";
 import { useUser } from "@stackframe/stack";
 import styles from "./navBar.module.css";
 
 export default function NavBar() {
-    const user = useUser({ or: 'redirect' });
+    const user = useUser({ or: "redirect" });
     const navLinks = [
         { href: "/app/jobs-manager", label: "Jobs Manager" },
         { href: "/app/titles-manager", label: "Titles Manager" },
@@ -16,26 +16,57 @@ export default function NavBar() {
         { href: "/about", label: "About" },
     ];
     const pathname = usePathname();
+    const [selected, setSelected] = React.useState(undefined);
+
+    // Prevent showing loader when a modal that changes url is open
+    if (selected !== undefined && pathname.includes(selected)) {
+        setSelected(undefined);
+    }
 
     return (
         <nav className={styles.navLinksContainer}>
             <div className={styles.navLinksDiv}>
-                {navLinks.map(link => (
-                    <Link href={link.href} key={link.href}
-                        className={`${styles.navLink} primary-btn`}
+                {navLinks.map((link) => (
+                    <Link
+                        href={link.href}
+                        key={link.label}
+                        className={`${styles.navLink} primary-btn
+                            ${
+                                selected === link.href &&
+                                !pathname.includes(link.href)
+                                    ? "loader-bar-fast animate-faster"
+                                    : ""
+                            }`}
                         style={{
-                            color: pathname == link.href ? "white" : "var(--foreground)",
-                            borderColor: pathname == link.href ? "var(--foreground)" : "var(--background)",
-                            backgroundColor: pathname == link.href ? "#171717" : "var(--background)"
+                            color:
+                                pathname == link.href
+                                    ? "white"
+                                    : "var(--foreground)",
+                            borderColor:
+                                pathname == link.href
+                                    ? "var(--foreground)"
+                                    : "var(--background)",
+                            backgroundColor:
+                                pathname == link.href
+                                    ? "#171717"
+                                    : "var(--background)",
                         }}
-                    >{link.label}</Link>
+                        onClick={() => {
+                            setSelected(link.href);
+                        }}
+                    >
+                        {link.label}
+                    </Link>
                 ))}
             </div>
             <div className={styles.navUserSection}>
                 <button
                     className="secondary-btn"
-                    onClick={() => user?.signOut()}>Leave</button>
+                    onClick={() => user?.signOut()}
+                >
+                    Leave
+                </button>
             </div>
         </nav>
-    )
+    );
 }
